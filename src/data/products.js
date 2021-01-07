8f4c71e42cd0adb23868023ec66a92dc1974383e
rkorzhoff <r.korzhoff@gmail.com>
2021-01-07 23:04:53 +0300

    first commit

---
+++ src/data/products.js
@@ -0,0 +1,47 @@
+export default [
+  {
+    title: 'Apple EarPods',
+    price: 890,
+    image: '/new-img/earpods.jpg',
+  },
+  {
+    title: 'Графический планшет WACOM One Small',
+    price: 3649,
+    image: '/new-img/wacom-one-small.jpg',
+  },
+  {
+    title: 'Электронная книга Amazon Kindle PaperWhite',
+    price: 10500,
+    image: '/new-img/amazon-kindle-paperwhite.jpg',
+  },
+  {
+    title: 'Умные часы Amazfit GTS',
+    price: 7810,
+    image: '/new-img/amazfit.jpg',
+  },
+  {
+    title: 'Геймпад Sony DualSense',
+    price: 4948,
+    image: '/new-img/dualsense.jpg',
+  },
+  {
+    title: 'Cyberpunk 2077',
+    price: 1190,
+    image: '/new-img/cyberpunk.jpg',
+  },
+  {
+    title: 'Планшет Samsung Galaxy Tab A7',
+    price: 16890,
+    image: '/new-img/galaxytab.jpg',
+  },
+  {
+    title: 'Умные часы Samsung Galaxy Watch',
+    price: 14190,
+    image: '/new-img/galaxywatch.jpg',
+  },
+  {
+    title: 'Беспроводные наушники JBL Tune 120 TWS',
+    price: 2498,
+    image: '/new-img/jbl.jpg',
+  },
+];
