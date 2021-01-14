6ddd88ddc7a536e143f9b3c2d4bb1910b15827d2
rkorzhoff <r.korzhoff@gmail.com>
2021-01-15 00:52:59 +0300

    pagination been added

---
+++ src/components/BasePagination.vue
@@ -0,0 +1,66 @@
+<template>
+  <ul class="catalog__pagination pagination">
+    <li class="pagination__item">
+      <a
+        class="pagination__link pagination__link--arrow "
+        aria-label="Предыдущая страница"
+        :class="{ 'pagination__link--disabled': isFirstPage === true }"
+        @click.prevent="
+          {
+            paginate(prevPage);
+          }
+        "
+      >
+        <svg width="8" height="14" fill="currentColor">
+          <use xlink:href="#icon-arrow-left"></use>
+        </svg>
+      </a>
+    </li>
+    <li class="pagination__item" v-for="pageNumber in pages" :key="pageNumber">
+      <a href="#" class="pagination__link " :class="{ 'pagination__link--current': pageNumber === page }" @click.prevent="paginate(pageNumber)">
+        {{ pageNumber }}
+      </a>
+    </li>
+    <li class="pagination__item">
+      <a class="pagination__link pagination__link--arrow" href="#" aria-label="Следующая страница" :class="{ 'pagination__link--disabled': isLastPage === true }" @click.prevent="paginate(nextPage)">
+        <svg width="8" height="14" fill="currentColor">
+          <use xlink:href="#icon-arrow-right"></use>
+        </svg>
+      </a>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  model: {
+    prop: 'page',
+    event: 'paginate',
+  },
+  props: ['page', 'count', 'perPage'],
+  computed: {
+    pages() {
+      return Math.ceil(this.count / this.perPage);
+    },
+    nextPage() {
+      if (this.page === this.pages) return this.pages;
+      return this.page + 1;
+    },
+    prevPage() {
+      if (this.page === 1) return 1;
+      return this.page - 1;
+    },
+    isFirstPage() {
+      return this.page === 1;
+    },
+    isLastPage() {
+      return this.page === this.pages;
+    },
+  },
+  methods: {
+    paginate(page) {
+      this.$emit('paginate', page);
+    },
+  },
+};
+</script>
