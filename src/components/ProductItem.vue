5e59a534e6f2a693137d8bb362f9bb833c185e47
rkorzhoff <r.korzhoff@gmail.com>
2021-01-16 01:43:26 +0300

    Id have been added to items

--- src/components/ProductItem.vue
+++ src/components/ProductItem.vue
@@ -13,19 +13,19 @@
     <ul class="colors colors--black">
       <li class="colors__item">
         <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio" name="color-1" value="#73B6EA" checked="" />
+          <input class="colors__radio sr-only" type="radio" value="#73B6EA" v-model="color" />
           <span class="colors__value" style="background-color: #73b6ea"> </span>
         </label>
       </li>
       <li class="colors__item">
         <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio" name="color-1" value="#8BE000" />
+          <input class="colors__radio sr-only" type="radio" value="#8BE000" v-model="color" />
           <span class="colors__value" style="background-color: #8be000"> </span>
         </label>
       </li>
       <li class="colors__item">
         <label class="colors__label">
-          <input class="colors__radio sr-only" type="radio" name="color-1" value="#222" />
+          <input class="colors__radio sr-only" type="radio" value="#222" v-model="color" />
           <span class="colors__value" style="background-color: #222"> </span>
         </label>
       </li>
@@ -34,6 +34,11 @@
 </template>
 <script>
 export default {
+  data() {
+    return {
+      color: '#73B6EA',
+    };
+  },
   props: ['product'],
 };
 </script>
--- src/data/products.js
+++ src/data/products.js
@@ -1,45 +1,54 @@
 export default [
   {
+    id: 1,
     title: 'Apple EarPods',
     price: 890,
     image: '/new-img/earpods.jpg',
   },
   {
+    id: 2,
     title: 'Графический планшет WACOM One Small',
     price: 3650,
     image: '/new-img/wacom-one-small.jpg',
   },
   {
+    id: 3,
     title: 'Электронная книга Amazon Kindle PaperWhite',
     price: 10500,
     image: '/new-img/amazon-kindle-paperwhite.jpg',
   },
   {
+    id: 4,
     title: 'Умные часы Amazfit GTS',
     price: 7810,
     image: '/new-img/amazfit.jpg',
   },
   {
+    id: 5,
     title: 'Геймпад Sony DualSense',
     price: 4948,
     image: '/new-img/dualsense.jpg',
   },
   {
+    id: 6,
     title: 'Cyberpunk 2077',
     price: 1190,
     image: '/new-img/cyberpunk.jpg',
   },
   {
+    id: 7,
     title: 'Планшет Samsung Galaxy Tab A7',
     price: 16890,
     image: '/new-img/galaxytab.jpg',
   },
   {
+    id: 8,
     title: 'Умные часы Samsung Galaxy Watch',
     price: 14190,
     image: '/new-img/galaxywatch.jpg',
   },
   {
+    id: 9,
     title: 'Беспроводные наушники JBL Tune 120 TWS',
     price: 2498,
     image: '/new-img/jbl.jpg',
